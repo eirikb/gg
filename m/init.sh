3f401b9c07a79fe166b41a22a153b93995695da4
Eirik Brandtzæg <eirikb@eirikb.no>
2021-10-29 21:38:29 +0000

    Feature/init (#3)
    Init!

--- m/init.sh
+++ m/init.sh
@@ -1,19 +1,14 @@
 cd .cache/m || exit
 for gg in gg*
 do
-  echo "Running $gg..."
-  file "$gg"
-  ldd "$gg"
   chmod +x "$gg"
   # shellcheck disable=SC2086
-  if "./$gg"; then
-    file mn
-    ldd mn
+  if "./$gg" 2> /dev/null; then
     chmod +x mn
     ./mn -- "$USER_PWD" $1
     exit
   fi
 done
 
-echo "Failed?!"
+echo "Your system is not supported"
 exit 1
