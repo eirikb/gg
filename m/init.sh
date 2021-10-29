4a3c5aa0a2cf447fbf21b4955b007ac288362ddf
Eirik Brandtzæg <eirikb@eirikb.no>
2021-10-29 23:27:18 +0200

    Test

--- m/init.sh
+++ m/init.sh
@@ -1,15 +1,9 @@
 cd .cache/m || exit
 for gg in gg*
 do
-  echo "Running $gg..."
-  file "$gg"
-  ldd "$gg"
   chmod +x "$gg"
   # shellcheck disable=SC2086
   if "./$gg"; then
-    file mn
-    ldd mn
-    chmod +x mn
     ./mn -- "$USER_PWD" $1
     exit
   fi
