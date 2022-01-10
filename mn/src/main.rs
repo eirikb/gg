6f4b223b705564dd5e8f6a3223d735e13057d674
Eirik Brandtzæg <eirikb@eirikb.no>
2022-01-10 22:33:35 +0100

    Test build

--- mn/src/main.rs
+++ mn/src/main.rs
@@ -1,3 +1,16 @@
+extern crate clap;
+
+use clap::{App, SubCommand};
+
 fn main() {
+    let app = App::new("m")
+        .version("1.0")
+        .author("Eirik Brandtzæg. <eirikb@eirikb.no>")
+        .about("Bootstrap")
+        .subcommand(SubCommand::with_name("node")
+            .about("Ugh node"));
+    let matches = app.get_matches();
+
+    let val = matches.value_of("node").unwrap_or("OK");
     println!("Hello, world!");
 }
