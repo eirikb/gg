7dc29bc73e3ee38e0d1aaeccedd6b8597ddd83e8
Eirik Brandtzæg <eirikb@eirikb.no>
2022-08-04 18:39:40 +0200

    Test build

--- mn/src/main.rs
+++ mn/src/main.rs
@@ -3,16 +3,16 @@ extern crate clap;
 use clap::{App, SubCommand};
 
 fn main() {
-    let app = App::new("m")
-        .version("1.0")
-        .author("Eirik Brandtzæg. <eirikb@eirikb.no>")
-        .about("Bootstrap")
-        .subcommand(SubCommand::with_name("node")
-            .about("Ugh node"))
-        .subcommand(SubCommand::with_name("")
-            .about("Ugh no"));
-    let matches = app.get_matches();
-
-    let val = matches.value_of("node").unwrap_or("OK");
+    // let app = App::new("m")
+    //     .version("1.0")
+    //     .author("Eirik Brandtzæg. <eirikb@eirikb.no>")
+    //     .about("Bootstrap")
+    //     .subcommand(SubCommand::with_name("node")
+    //         .about("Ugh node"))
+    //     .subcommand(SubCommand::with_name("")
+    //         .about("Ugh no"));
+    // let matches = app.get_matches();
+    //
+    // let val = matches.value_of("node").unwrap_or("OK");
     println!("Hello, world!");
 }
