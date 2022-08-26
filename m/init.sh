b4a619babda63bed77de18ed24fdc28a44fe3287
Eirik Brandtzæg <eirikb@eirikb.no>
2022-08-26 21:25:29 +0200

    Build

--- m/init.sh
+++ m/init.sh
@@ -1,11 +1,10 @@
 cd .cache/m || exit
-for gg in gg*
-do
+for gg in .cache/m/gg*; do
   chmod +x "$gg"
   # shellcheck disable=SC2086
-  if "./$gg" 2> /dev/null; then
+  if "./$gg" 2>/dev/null; then
     chmod +x mn
-    ./mn -- "$USER_PWD" $1
+    ./mn "$@"
     exit
   fi
 done
--- m/kick.sh
+++ m/kick.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -f ./.cache/m/mn ]; then
-  ./.cache/m/mn
+  ./.cache/m/mn "$@"
   exit
 fi
-tail -c +128 "$0" | tar -zpx  && ./.cache/m/init.sh;
-exit;
+tail -c +130 "$0" | tar -zpx && ./.cache/m/init.sh
+exit
