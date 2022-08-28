5fac9aac90083edb4940ea3c8ffbd73913a7ebfb
Eirik Brandtzæg <eirikb@eirikb.no>
2022-08-28 15:26:31 +0200

    Build

--- m/init.sh
+++ m/init.sh
@@ -3,22 +3,9 @@ for gg in gg*; do
   chmod +x "$gg"
   # shellcheck disable=SC2086
   if "./$gg" 2>/dev/null; then
-    echo "gg is $gg"
     chmod +x mn
     echo "$gg" >system
-    ls -lah
-    echo "system?"
-    cat system
     ./mn "$@"
-    echo 1
-    pwd
-    cd ..
-    echo 1
-    pwd
-    cd ..
-    echo 1
-    pwd
-    ./.cache/m/mn "$@"
     exit
   fi
 done
