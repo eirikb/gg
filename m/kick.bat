3115ef1eb78b5901246cde77c675f3f898bda49f
Eirik Brandtzæg <>
2022-08-29 22:17:16 +0200

    Build

--- m/kick.bat
+++ m/kick.bat
@@ -1,7 +1,7 @@
 : <<BATCH
     @echo off
-    powershell -c "sc m2 ([byte[]](gc m.cmd -Encoding Byte | select -Skip 331)) -Encoding Byte"
+    powershell -c "sc m2 ([byte[]](gc m.cmd -Encoding Byte | select -Skip 361)) -Encoding Byte"
     tar -zxf m2
-    powershell .cache\m\init.ps1 %s
+    powershell -executionpolicy bypass -file .cache\m\init.ps1 %s
     exit /b
 BATCH
