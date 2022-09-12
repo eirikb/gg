08a07e7aea09c3665c128920e12cc1fd53961966
Eirik Brandtzæg <>
2022-09-12 17:18:01 +0200

    Build

--- m/init.ps1
+++ m/init.ps1
@@ -1,7 +1,16 @@
 echo "Hello..."
 
-cd .cache/m
-ls *gg*.exe | % {
-    & $_
-    & .\mn "$@"
+cd .cache\m
+ls gg* | % {
+    $name = $_ + ".exe"
+    if (!$_.EndsWith(".exe")) {
+        Write-Host "re to the name"
+        cp $_ $name
+        & $name
+        & .\mn "$@"
+    } else {
+        Write-Host $_
+        & $_
+        & .\mn "$@"
+    }
 }
