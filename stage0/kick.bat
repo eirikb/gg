d31b280eea6b6972320cf8c01d7ef7d21feae917
Eirik Brandtzæg <>
2022-09-12 22:28:06 +0200

    Build!

--- stage0/kick.bat
+++ stage0/kick.bat
@@ -1,6 +1,6 @@
 : <<BATCH
     @echo off
-    powershell -c "sc m2 ([byte[]](gc m.cmd -Encoding Byte | select -Skip 361)) -Encoding Byte"
+    powershell -c "sc m2 ([byte[]](gc gg.cmd -Encoding Byte | select -Skip 363)) -Encoding Byte"
     tar -zxf m2
     powershell -executionpolicy bypass -file .cache\m\init.ps1 %s
     exit /b
--- stage0/kick.sh
+++ stage0/kick.sh
@@ -3,5 +3,5 @@ if [ -f ./.cache/m/init.sh ]; then
   exit
 fi
 
-tail -c +362 m.cmd | tar -zpx && ./.cache/m/init.sh "$@"
+tail -c +364 gg.cmd | tar -zpx && ./.cache/m/init.sh "$@"
 exit
