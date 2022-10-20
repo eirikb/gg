819dc8f8c953f985e3bdc312dacd024553867c1a
Eirik Brandtzæg <eirikb@eirikb.no>
2022-10-20 19:56:53 +0200

    Stagus

--- stage1/stage1.bat
+++ stage1/stage1.bat
@@ -1,7 +1,7 @@
 : <<BATCH
     @echo off
-    powershell -c "sc m2 ([byte[]](gc gg.cmd -Encoding Byte | select -Skip 367)) -Encoding Byte"
+    powershell -c "sc m2 ([byte[]](gc gg.cmd -Encoding Byte | select -Skip 369)) -Encoding Byte"
     tar -zxf m2
-    powershell -executionpolicy bypass -file .cache\gg\init.ps1 %s
+    powershell -executionpolicy bypass -file .cache\gg\stage2.ps1 %s
     exit /b
 BATCH
