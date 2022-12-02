6a52781884c9a94d362fa37000df618e7f750a79
Eirik Brandtzæg <eirikb@eirikb.no>
2022-12-02 23:41:13 +0100

    Java, one step closer

--- stage4/src/java.rs
+++ stage4/src/java.rs
@@ -32,18 +32,15 @@ pub struct Root2 {
     pub os: String,
     #[serde(rename = "release_status")]
     pub release_status: String,
-    #[serde(rename = "sha256_hash")]
-    pub sha256_hash: String,
     #[serde(rename = "support_term")]
     pub support_term: String,
     pub url: String,
-    #[serde(rename = "zulu_version")]
-    pub zulu_version: Vec<i64>,
 }
 
 
 pub async fn get_java_download_url(target: &target::Target) -> String {
-    let json = reqwest::get("https://www.azul.com/wp-admin/admin-ajax.php?action=bundles&endpoint=community&use_stage=false&include_fields=java_version%2Copenjdk_build_number%2Crelease_status%2Csupport_term%2Cos%2Carch%2Chw_bitness%2Cabi%2Cbundle_type%2Cjavafx%2Clatest%2Cext%2Cname%2Csha256_hash%2Curl%2Ccpu_gen%2Cfeatures").await.unwrap().text().await.unwrap();
+    let json = reqwest::get("https://www.azul.com/wp-admin/admin-ajax.php?action=bundles&endpoint=community&use_stage=false&include_fields=java_version,release_status,abi,arch,bundle_type,cpu_gen,ext,features,hw_bitness,javafx,latest,os,support_term").await.unwrap().text().await.unwrap();
+    println!("{}", json);
     let root: Root = serde_json::from_str(json.as_str()).expect("JSON was not well-formatted");
     let node = root.iter().find(|node| {
         let node_os = match node.os.as_str() {
@@ -51,16 +48,20 @@ pub async fn get_java_download_url(target: &target::Target) -> String {
             "linux" => Os::Linux,
             _ => Os::Mac,
         };
+        let ext = match target.os {
+            Os::Windows => "zip",
+            _ => "tar.gz",
+        };
         let node_arch = match (node.arch.as_str(), node.hw_bitness.as_str()) {
             ("x86", "64") => Some(Arch::X86_64),
             ("arm", "64") => Some(Arch::Armv7),
             _ => None
         };
         if node_arch.is_some() {
-            node_os == target.os && node_arch.unwrap() == target.arch && node.javafx
+            node_os == target.os && node_arch.unwrap() == target.arch && node.javafx && node.ext == ext
         } else {
             false
         }
     });
-    return String::from("what now");
+    return String::from(node.unwrap().clone().url);
 }
