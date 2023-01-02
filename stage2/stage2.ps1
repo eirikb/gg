f8f4d3450d09a5edb2609a3c1e9196fc78a071ba
Eirik Brandtzæg <eirikb@eirikb.no>
2023-01-02 22:26:56 +0100

    batch in blind

--- stage2/stage2.ps1
+++ stage2/stage2.ps1
@@ -1,14 +1,21 @@
 echo "Hello..."
 
 if (Test-Path .cache\gg\stage4) {
+    Write-Host "Run 1";
     return Start-Process .cache\gg\stage4 $args;
 }
 
+ls
+Write-Host "cd..."
 cd .cache\gg
+ls
+pwd
 Get-Item stage3* | % {
+    pwd
     $name = $_.Name + ".exe"
     Write-Host $_.Name
     Write-Host "Hello " + $_.Name
+    Test-Path $_.Name
     if (!$_.Name.EndsWith(".exe")) {
         Write-Host "re to the name"
         cp $_.Name $name
@@ -22,6 +29,7 @@ Get-Item stage3* | % {
 }
 
 if (Test-Path .cache\gg\stage4) {
+    Write-Host "Run 2";
     return Start-Process .cache\gg\stage4 $args;
 } else {
     Write-Host "stage4 not found :(";
