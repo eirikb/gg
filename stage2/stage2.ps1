9a9bca31642ac7f4cb2c80e58ca461901b8ea77f
Eirik Brandtzæg <eirikb@eirikb.no>
2023-01-06 22:59:43 +0100

    h

--- stage2/stage2.ps1
+++ stage2/stage2.ps1
@@ -34,6 +34,9 @@ cd ../..
 if (Test-Path ".cache\gg\stage4") {
     Write-Host "Run 2"
     cat .cache\gg\system
+    echo location is "$(Get-Location)"
+    echo "location is $(Get-Location)"
+    echo "args is $args"
     $proc = Start-Process ".\.cache\gg\stage4" -WorkingDirectory "$(Get-Location)" -PassThru -NoNewWindow -ErrorAction SilentlyContinue -ArgumentList $args
     Wait-Process -InputObject $proc
     return $proc.ExitCode
