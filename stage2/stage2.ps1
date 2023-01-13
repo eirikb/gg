f381600abe0fe74378f05ec417b2e40d0982fa6d
Eirik Brandtzæg <eirikb@eirikb.no>
2023-01-13 22:16:38 +0100

    Build

--- stage2/stage2.ps1
+++ stage2/stage2.ps1
@@ -22,31 +22,7 @@ Get-Item stage3* | % {
     $proc = Start-Process ".\$name" -PassThru -NoNewWindow -ErrorAction SilentlyContinue
     Wait-Process -InputObject $proc
     if ($proc.ExitCode -eq 0) {
-        $_.Name > system
         Out-File -Encoding ascii -LiteralPath system -InputObject $_.Name
-        Out-File -Encoding utf8BOM -LiteralPath system2 -InputObject $_.Name
-        Out-File -Encoding utf8NoBom -LiteralPath system3 -InputObject $_.Name
-        Out-File -Encoding utf8 -LiteralPath system4 -InputObject $_.Name
-
-        echo "GO!"
-        cat system
-        echo "... 1"
-        cat system2
-        echo "... 2"
-        cat system3
-        echo "... 3"
-        cat system4
-        echo "... 4"
-        Write-Host $([System.IO.File]::ReadAllBytes("$(pwd)\system"))
-        echo "... 5"
-        Write-Host $([System.IO.File]::ReadAllBytes("$(pwd)\system2"))
-        echo "... 6"
-        Write-Host $([System.IO.File]::ReadAllBytes("$(pwd)\system3"))
-        echo "... 7"
-        Write-Host $([System.IO.File]::ReadAllBytes("$(pwd)\system4"))
-        echo "TEST"
-        echo $([system.Text.Encoding]::UTF8.GetBytes("test"))
-        echo $([system.Text.Encoding]::UTF8.GetBytes($_.Name))
     } else {
         Write-Host "Didn't work $($proc.ExitCode)"
         Write-Host "proc is $proc"
