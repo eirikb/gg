6d2f7d55eb026668906f75598e11c32c7bf33bf6
Eirik Brandtzæg <eirikb@eirikb.no>
2023-01-20 15:41:21 +0100

    ver

--- stage1/stage1.bat
+++ stage1/stage1.bat
@@ -1,7 +1,7 @@
 : <<BATCH
     @echo off
     : VERSION:
-    powershell -c "sc m2 ([byte[]](gc gg.cmd -Encoding Byte | select -Skip 419)) -Encoding Byte"
+    powershell -c "sc m2 ([byte[]](gc gg.cmd -Encoding Byte | select -Skip 417)) -Encoding Byte"
     tar -zxf m2
     powershell -executionpolicy bypass -file .cache\gg\stage2.ps1 %*
     exit /b
