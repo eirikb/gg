43e376e6f5eaead5f559235adb11abcd432e3855
Eirik Brandtzæg <eirikb@eirikb.no>
2023-01-20 22:43:07 +0100

    No clap

--- stage4/src/main.rs
+++ stage4/src/main.rs
@@ -1,10 +1,7 @@
-use std::collections::HashMap;
 use std::fs;
-use std::process::ExitCode;
+use std::process::{ExitCode};
 
 use log::{debug, info};
-use regex::Regex;
-use semver::VersionReq;
 
 use bloody_indiana_jones::download_unpack_and_all_that_stuff;
 
@@ -23,42 +20,65 @@ mod executor;
 mod no_clap;
 
 
+fn print_help(ver: &str) {
+    println!(r"gg.cmd - The Ultimate Executable Manager
+https://github.com/eirikb/gg
+
+Version: {ver}
+
+Usage: ./gg.cmd [options] <executable name>@<version>:<dependent executable name>@<version> [program arguments]
+
+Options:
+    -v          Verbose output
+    -vv         Debug output
+    -e          Execute custom command. Note that you can pass arguments at the end
+    -h          Print help
+    -V          Print version
+
+Examples:
+    ./gg.cmd node
+    ./gg.cmd -c soapui java@17
+    ./gg.cmd gradle@6:java@17 clean build
+    ./gg.cmd node@10 -e 'console.log(1)'
+    ./gg.cmd -vv npm@14 start
+
+Supported systems:
+    node (npm, npx will also work, version refers to node version)
+    gradle
+    java
+");
+}
+
 #[tokio::main]
 async fn main() -> ExitCode {
     let ver = option_env!("VER").unwrap_or("dev");
-    println!("Version is {ver}");
 
-    if let Some(no_clap) = NoClap::new() {
-        let log_level = vec![("-vv", "debug"), ("-v", "info")].into_iter().find(|(input, _)| no_clap.gg_args.contains(&input.to_string()));
+    let no_clap = NoClap::new();
+    env_logger::init_from_env(env_logger::Env::default().default_filter_or(&no_clap.log_level));
 
-        let log_level = if let Some((_, log_level)) = log_level {
-            log_level
-        } else {
-            "warn"
-        };
+    if no_clap.help {
+        print_help(ver);
+        return ExitCode::from(0);
+    }
 
-        env_logger::init_from_env(env_logger::Env::default().default_filter_or(log_level));
+    if no_clap.version {
+        println!("{}", ver);
+        return ExitCode::from(0);
+    }
 
-        debug!(target: "main", "{:?}", &no_clap);
+    debug!(target: "main", "{:?}", &no_clap);
 
-        let system = fs::read_to_string("./.cache/gg/system").unwrap_or(String::from("x86_64-linux")).trim().to_string();
-        info!("System is {system}");
+    let system = fs::read_to_string("./.cache/gg/system").unwrap_or(String::from("x86_64-linux")).trim().to_string();
+    info!("System is {system}");
 
-        let target = target::parse_target(&system);
-        debug!(target: "main", "{:?}", &target);
+    let target = target::parse_target(&system);
+    debug!(target: "main", "{:?}", &target);
 
-        let version_reqs_iter = no_clap.cmds.split(":").map(|cmd| {
-            let parts: Vec<_> = Regex::new(r"@").unwrap().split(cmd).into_iter().collect();
-            let cmd = parts[0].to_string();
-            let version_req = VersionReq::parse(parts.get(1).unwrap_or(&"")).ok();
-            (cmd, version_req)
-        });
+    debug!(target: "main", "{:?}", &no_clap.version_req_map);
 
-        let mut version_reqs: Vec<(String, Option<VersionReq>)> = version_reqs_iter.clone().collect();
-        let (cmd, _) = version_reqs.remove(0);
-        let version_req_map: HashMap<String, Option<VersionReq>> = version_reqs_iter.into_iter().collect();
-        debug!(target: "main", "{:?}", &version_req_map);
+    let version_req_map = no_clap.version_req_map;
 
+    return if let Some(cmd) = no_clap.cmd {
         let executor: Option<Box<dyn Executor>> = match cmd.as_str() {
             "node" | "npm" | "npx" => Some(Box::new(Node { cmd, version_req_map })),
             "gradle" => Some(Box::new(Gradle { version_req_map })),
@@ -75,9 +95,10 @@ async fn main() -> ExitCode {
         } else {
             info!("Unable to find an executor");
         }
+        ExitCode::from(1)
     } else {
-        println!("No command");
-        println!("Here be help in the future. I promise");
+        println!("Missing command");
+        print_help(ver);
+        ExitCode::from(1)
     }
-    return ExitCode::from(1);
 }
