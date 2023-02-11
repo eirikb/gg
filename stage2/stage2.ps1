a8a4a8bb477577f84a1754c1b432c0973efc8927
Eirik Brandtzæg <eirikb@eirikb.no>
2023-02-11 15:28:14 +0000

    Build

--- stage2/stage2.ps1
+++ stage2/stage2.ps1
@@ -1,43 +1,56 @@
 Write-Host "Time to GOGOGO"
 
-ls .cache
-cat .cache/gg/hashes
-#$proc = Start-Process ".\.cache\gg\stage4" -WorkingDirectory "$(Get-Location)" -PassThru -NoNewWindow -ErrorAction SilentlyContinue -ArgumentList $args
-#Wait-Process -InputObject $proc
+$stage4 = ".cache\gg\stage4.exe"
 
-#if (Test-Path .cache\gg\stage4) {
-#    return Start-Process .cache\gg\stage4 $args
-#}
+function Run()
+{
+    echo run
+    $htArgs = if ($Args.Count)
+    {
+        @{ Args = $Args }
+    }
+    else
+    {
+        @{ }
+    }
+    Write-Host $htArgs
+    Write-Host $stage4
+    $proc = Start-Process $stage4 -WorkingDirectory "$( Get-Location )" -PassThru -NoNewWindow -ErrorAction SilentlyContinue -ArgumentList $htArgs
+    echo $proc
+    Wait-Process -InputObject $proc
+    exit $proc.ExitCode
+}
 
-#cd .cache\gg
-#Get-Item stage3* | % {
-#    pwd
-#    $name = $_.Name + ".exe"
-#    Test-Path $_.Name
-#    if (!$_.Name.EndsWith(".exe")) {
-#        cp $_.Name $name
-#    }
-#    $proc = Start-Process ".\$name" -PassThru -NoNewWindow -ErrorAction SilentlyContinue
-#    Wait-Process -InputObject $proc
-#    if ($proc.ExitCode -eq 0) {
-#        Out-File -Encoding ascii -LiteralPath system -InputObject $_.Name
-#    }
-#}
-#
-#cd ../..
-#
-#if (Test-Path ".cache\gg\stage4") {
-#    cat .cache\gg\system
-#    $proc = Start-Process ".\.cache\gg\stage4" -WorkingDirectory "$(Get-Location)" -PassThru -NoNewWindow -ErrorAction SilentlyContinue -ArgumentList $args
-#    Wait-Process -InputObject $proc
-#
-#    $proc = Start-Process ".\.cache\gg\stage4" -WorkingDirectory "$(Get-Location)" -NoNewWindow -ErrorAction SilentlyContinue -ArgumentList $args
-#    Wait-Process -InputObject $proc
-#
-#    $htArgs  = if ($Args.Count) { @{ Args = $Args } } else { @{} }
-#    $proc = Start-Process ".\.cache\gg\stage4" -WorkingDirectory "$(Get-Location)" -NoNewWindow -ErrorAction SilentlyContinue -ArgumentList $htArgs
-#    Wait-Process -InputObject $proc
-#} else {
-#    Write-Host "stage4 not found :("
-#}
-#
+if (Test-Path $stage4)
+{
+    Run
+}
+
+$arch = $Env:PROCESSOR_ARCHITECTURE
+if ($arch -Eq "AMD64")
+{
+    $arch = "x86_64"
+}
+
+$hashes = (Get-Content .cache/gg/hashes).Split("`n")
+Write-Host $hashes
+$hash = ($hashes | Where-Object { $_ -match "$arch.*windows" })
+if ($hash)
+{
+    Write-Host "Found hash $hash"
+    $hash = $hash.split("=")[1]
+    Write-Host "Actual hash $hash"
+    Invoke-WebRequest "https://gg.eirikb.no/$hash" -OutFile $stage4
+    if (Test-Path $stage4)
+    {
+        Run
+    }
+    else
+    {
+        Write-Host "Unable to download. Try again"
+    }
+}
+else
+{
+    Write-Host "Hash not found :("
+}
