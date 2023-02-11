41cdc8215811e0d3c9248e93b256de117ebb5040
Eirik Brandtzæg <eirikb@eirikb.no>
2023-02-11 07:52:26 +0000

    Build

--- stage2/stage2.ps1
+++ stage2/stage2.ps1
@@ -1,36 +1,42 @@
-if (Test-Path .cache\gg\stage4) {
-    return Start-Process .cache\gg\stage4 $args
-}
+Write-Host "Time to GOGOGO"
 
-cd .cache\gg
-Get-Item stage3* | % {
-    pwd
-    $name = $_.Name + ".exe"
-    Test-Path $_.Name
-    if (!$_.Name.EndsWith(".exe")) {
-        cp $_.Name $name
-    }
-    $proc = Start-Process ".\$name" -PassThru -NoNewWindow -ErrorAction SilentlyContinue
-    Wait-Process -InputObject $proc
-    if ($proc.ExitCode -eq 0) {
-        Out-File -Encoding ascii -LiteralPath system -InputObject $_.Name
-    }
-}
-
-cd ../..
-
-if (Test-Path ".cache\gg\stage4") {
-    cat .cache\gg\system
+ls .cache
     $proc = Start-Process ".\.cache\gg\stage4" -WorkingDirectory "$(Get-Location)" -PassThru -NoNewWindow -ErrorAction SilentlyContinue -ArgumentList $args
     Wait-Process -InputObject $proc
 
-    $proc = Start-Process ".\.cache\gg\stage4" -WorkingDirectory "$(Get-Location)" -NoNewWindow -ErrorAction SilentlyContinue -ArgumentList $args
-    Wait-Process -InputObject $proc
+#if (Test-Path .cache\gg\stage4) {
+#    return Start-Process .cache\gg\stage4 $args
+#}
 
-    $htArgs  = if ($Args.Count) { @{ Args = $Args } } else { @{} }
-    $proc = Start-Process ".\.cache\gg\stage4" -WorkingDirectory "$(Get-Location)" -NoNewWindow -ErrorAction SilentlyContinue -ArgumentList $htArgs
-    Wait-Process -InputObject $proc
-} else {
-    Write-Host "stage4 not found :("
-}
-
+#cd .cache\gg
+#Get-Item stage3* | % {
+#    pwd
+#    $name = $_.Name + ".exe"
+#    Test-Path $_.Name
+#    if (!$_.Name.EndsWith(".exe")) {
+#        cp $_.Name $name
+#    }
+#    $proc = Start-Process ".\$name" -PassThru -NoNewWindow -ErrorAction SilentlyContinue
+#    Wait-Process -InputObject $proc
+#    if ($proc.ExitCode -eq 0) {
+#        Out-File -Encoding ascii -LiteralPath system -InputObject $_.Name
+#    }
+#}
+#
+#cd ../..
+#
+#if (Test-Path ".cache\gg\stage4") {
+#    cat .cache\gg\system
+#    $proc = Start-Process ".\.cache\gg\stage4" -WorkingDirectory "$(Get-Location)" -PassThru -NoNewWindow -ErrorAction SilentlyContinue -ArgumentList $args
+#    Wait-Process -InputObject $proc
+#
+#    $proc = Start-Process ".\.cache\gg\stage4" -WorkingDirectory "$(Get-Location)" -NoNewWindow -ErrorAction SilentlyContinue -ArgumentList $args
+#    Wait-Process -InputObject $proc
+#
+#    $htArgs  = if ($Args.Count) { @{ Args = $Args } } else { @{} }
+#    $proc = Start-Process ".\.cache\gg\stage4" -WorkingDirectory "$(Get-Location)" -NoNewWindow -ErrorAction SilentlyContinue -ArgumentList $htArgs
+#    Wait-Process -InputObject $proc
+#} else {
+#    Write-Host "stage4 not found :("
+#}
+#
