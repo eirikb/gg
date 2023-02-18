609f10d11d882a2be8d7feb0f7fd7e834e6c4b75
Eirik Brandtzæg <eirikb@eirikb.no>
2023-02-18 17:54:00 +0000

    Windows

--- stage4/src/bloody_indiana_jones.rs
+++ stage4/src/bloody_indiana_jones.rs
@@ -83,7 +83,7 @@ pub async fn download_unpack_and_all_that_stuff(url: &str, path: &str) {
             info!("Decompressing Zip");
             info!("Path is {}", &path);
             let part = path.split("/").last().unwrap_or("unknown");
-            let part_path = format!(".cache/node/{part}");
+            let part_path = format!(".cache/{part}");
             info!("path_path {}", &part_path);
             create_dir_all(&part_path).expect("Unable to create download dir");
             let target_dir = PathBuf::from(&part_path);
