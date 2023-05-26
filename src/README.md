eda24c3f6890bdbfe8ea28a795c6aa27510e628d
Eirik Brandtzæg <eirikb@eirikb.no>
2023-05-26 22:00:16 +0200

    New command: openapi (#48)
    * Hello README for stages
    
    * Add openapi command
    
    Actually openapi-generator-cli, but the name should be safe for this.
    
    * Add openapi to tests
    
    * Add openapi to tests

---
+++ src/README.md
@@ -0,0 +1,24 @@
+Stages?  
+gg.cmd is split into different stages.
+
+The gg.cmd-file itself is built up like this:
+
+1. stage1 batch script
+2. stage 1 shell script
+3. gzipped content
+    1. stage 3 for each arch/os/variant
+
+Roughly:
+
+* **Stage 1**: Shell- / Batch-script for running stage2, or extracting gg.cmd (most of the file is gzip), and then
+  execute stage 2.
+* **Stage 2**: Shell- / PowerShell-script to execute stage 4, or use stage 3 to download stage 3, or use the OS if
+  possible.
+* **Stage 3**: Binary for each arch/os/variant for downloading stage 4. For example one for Linux x64 glibc, and another
+  for Linux x64 musl. Building a static version with musl would be too big.
+  Cosmopolitan does not (at writing time) support ARM.
+* ** Stage 4**: rust-based CLI. Does the actual logic (download, extract, execute). Hosted externally. One for each
+  OS/arch.
+
+
+
---
+++ src/stage4/src/openapigenerator.rs
@@ -0,0 +1,62 @@
+use std::fs::{read_dir, rename};
+use std::future::Future;
+use std::pin::Pin;
+
+use which::{Error, which_in_global};
+
+use crate::bloody_maven::get_download_urls_from_maven;
+use crate::Executor;
+use crate::executor::{AppInput, AppPath, Download, ExecutorCmd};
+
+pub struct OpenAPIGenerator {
+    pub executor_cmd: ExecutorCmd,
+}
+
+impl Executor for OpenAPIGenerator {
+    fn get_executor_cmd(&self) -> &ExecutorCmd {
+        return &self.executor_cmd;
+    }
+
+    fn get_download_urls<'a>(&'a self, _input: &'a AppInput) -> Pin<Box<dyn Future<Output=Vec<Download>> + 'a>> {
+        get_download_urls_from_maven("openapitools", "openapi-generator-cli")
+    }
+
+    fn get_bin(&self, _input: &AppInput) -> Vec<&str> { vec!("openapi-generator-cli.jar") }
+
+    fn get_name(&self) -> &str {
+        "openapi"
+    }
+
+    fn get_deps(&self) -> Vec<&str> {
+        vec!("java")
+    }
+
+    fn get_custom_bin_path(&self, paths: &str) -> Option<String> {
+        which_in_global("java", Some(paths)).and_then(|mut s| s.next().ok_or(Error::CannotFindBinaryPath)).map(|s| s.to_str().unwrap().to_string()).ok()
+    }
+
+
+    fn get_additional_args(&self, app_path: &AppPath) -> Vec<String> {
+        if let Some(path) = app_path.bin.to_str().map(|s| s.to_string()) {
+            vec!("-jar".to_string(), path)
+        } else {
+            vec!()
+        }
+    }
+
+    fn post_prep(&self, cache_path: &str) {
+        let entries = read_dir(&cache_path);
+        if let Ok(entries) = entries {
+            entries.for_each(|entry| {
+                if let Ok(entry) = entry {
+                    if let Some(path_str) = entry.path().to_str() {
+                        dbg!(&path_str);
+                        if path_str.contains("openapi-generator-cli") && path_str.ends_with("jar") {
+                            rename(entry.path(), cache_path.to_string() + "/openapi-generator-cli.jar").unwrap();
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
