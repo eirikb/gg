1749ddb1e964d75e3f5efebda8666e5108145521
Eirik Brandtzæg <eirikb@eirikb.no>
2025-05-21 22:30:23 +0200

    Remove temporary m2 file after extraction in stage1.bat

--- src/stage1/stage1.bat
+++ src/stage1/stage1.bat
@@ -3,6 +3,7 @@
     : VERSION: VERVER
     powershell -c "sc m2 ([byte[]](gc gg.cmd -Encoding Byte | select -Skip AAA)) -Encoding Byte"
     tar -zxf m2
+    del m2
     powershell -executionpolicy bypass -file .cache\gg\gg-VERVER\stage2.ps1 %*
     exit /b
 BATCH
