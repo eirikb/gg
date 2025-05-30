2ce799cae4caeba26f30c25f1d57b69a857167e3
Eirik Brandtzæg <eirikb@eirikb.no>
2025-05-30 23:11:46 +0200

    Adjust the darn local handling in stage1.bat

--- src/stage1/stage1.bat
+++ src/stage1/stage1.bat
@@ -24,8 +24,16 @@
     if not exist "%cacheBase%" mkdir "%cacheBase%"
     
     powershell -c "sc m2 ([byte[]](gc gg.cmd -Encoding Byte | select -Skip AAA)) -Encoding Byte"
-    tar -xzC "%cacheBase%" -f m2
+    tar -zxf m2
     del m2
+    
+    if "%localCache%"=="false" (
+        if exist ".cache\gg\gg-VERVER" (
+            xcopy /E /I /Y ".cache\gg\gg-VERVER" "%cacheDir%"
+            rmdir /S /Q ".cache"
+        )
+    )
+    
     powershell -executionpolicy bypass -file "%cacheDir%\stage2.ps1" %*
     exit /b %errorlevel%
 BATCH
