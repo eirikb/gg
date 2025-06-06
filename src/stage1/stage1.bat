ab419e941c1f4a4fb52eba358d34feb5aae9dc01
Eirik Brandtzæg <eirikb@eirikb.no>
2025-06-06 14:40:29 +0200

    Feature/eirikb/winwin (#134)
    * If stage2.ps1 is already extracted then execute it!
    
    * Fix passing of arguments to stage4 from stage2.ps1

--- src/stage1/stage1.bat
+++ src/stage1/stage1.bat
@@ -1,6 +1,10 @@
 : <<BATCH
     @echo off
     : VERSION: VERVER
+    if exist .cache\gg\gg-VERVER\stage2.ps1 (
+        powershell -executionpolicy bypass -file .cache\gg\gg-VERVER\stage2.ps1 %*
+        exit /b %errorlevel%
+    )
     powershell -c "sc m2 ([byte[]](gc gg.cmd -Encoding Byte | select -Skip AAA)) -Encoding Byte"
     tar -zxf m2
     del m2
