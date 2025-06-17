d9b12c3cf93330ebfbe3283ee1e00fda0c33c46a
Eirik Brandtzæg <eirikb@eirikb.no>
2025-06-17 10:18:57 +0200

    Fix running gg.cmd from different folder (#145)

--- src/stage1/stage1.bat
+++ src/stage1/stage1.bat
@@ -16,7 +16,7 @@
         exit /b %errorlevel%
     )
     if not exist "%GG_CACHE_DIR%" mkdir "%GG_CACHE_DIR%"
-    powershell -c "sc m2 ([byte[]](gc gg.cmd -Encoding Byte | select -Skip AAAA)) -Encoding Byte"
+    powershell -c "sc m2 ([byte[]](gc '%0' -Encoding Byte | select -Skip AAAA)) -Encoding Byte"
     tar -zxf m2 -C "%GG_CACHE_DIR%"
     del m2
     powershell -executionpolicy bypass -file "%GG_CACHE_DIR%\gg-VERVER\stage2.ps1" %*
